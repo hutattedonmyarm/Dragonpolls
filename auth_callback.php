72fb3ede132052bff1bb4c38029d0ffd4a55e72a
aymm <max.nuding@icloud.com>
2021-05-10 21:27:33 +0200

    #21 redirect to source page if parameter is set

--- auth_callback.php
+++ auth_callback.php
@@ -5,8 +5,13 @@ require_once __DIR__ .'/bootstrap.php';
 
 if (isset($_GET['code'])) {
   $success = $api->authenticate($_GET['code']);
+  $from = empty($_GET['from']) ? null : $_GET['from'];
+  $page = 'index.php';
+  if (!empty($_GET['from'])) {
+    $page = $_GET['from'];
+  }
   if ($success) {
-    redirect('index.php');
+    redirect($page);
   } else {
     quit(get_page_header().'Echo error authenticating');
   }
